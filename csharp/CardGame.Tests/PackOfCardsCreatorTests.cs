a62a1f3a1b6992a9508ea85a878186d45179efb7
Kyle Miller <kylemiller995@gmail.com>
2021-02-15 23:10:13 +0000

    add pack of cards creator, and unit tests

---
+++ csharp/CardGame.Tests/PackOfCardsCreatorTests.cs
@@ -0,0 +1,36 @@
+﻿using CardGame.Tests.Utilities;
+using CodingChallenge.CardGame;
+using NUnit.Framework;
+using System.Linq;
+
+namespace CardGame.Tests
+{
+    [TestFixture]
+    internal class PackOfCardsCreatorTests
+    {
+        private IPackOfCards deck; 
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var packOfCardsCreator = new PackOfCardsCreator();
+            deck = packOfCardsCreator.Create();
+        }
+
+        [Test]
+        public void Deck_has_52_cards()
+        {
+            Assert.That(deck.Count, Is.EqualTo(52));
+        }
+
+        [Test]
+        public void Deck_has_all_combinations_of_52_cards()
+        {
+            var sortedCards = SortedListOfCardsGenerator.Generate();
+            foreach(var card in deck)
+            {
+                Assert.That(sortedCards.Count(c => c.Suit == card.Suit && c.Value == card.Value), Is.EqualTo(1));
+            }
+        }
+    }
+}
---
+++ csharp/src/CardGame/PackOfCardsCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingChallenge.CardGame
+{
+    public class PackOfCardsCreator : IPackOfCardsCreator
+    {
+        public IPackOfCards Create()
+        {
+            var pack = new List<ICard>();
+            for(int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    pack.Add(new Card((Value)j, (Suit)i));
+                }
+            }
+            return new PackOfCards(new Random(), pack);
+        }
+    }
+}
